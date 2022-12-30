fe8566518c6507034fc64a48574e814095459a1f
Thomas Gehrmann <s6thgehr@gmail.com>
2022-12-30 21:10:58 +0100

    Add InitializeMint instruction

--- src/processor.rs
+++ src/processor.rs
@@ -7,12 +7,19 @@ use solana_program::{
     borsh::try_from_slice_unchecked,
     entrypoint::ProgramResult,
     msg,
+    native_token::LAMPORTS_PER_SOL,
     program::invoke_signed,
     program_error::ProgramError,
     program_pack::IsInitialized,
     pubkey::Pubkey,
     system_instruction,
-    sysvar::{rent::Rent, Sysvar},
+    system_program::ID as SYSTEM_PROGRAM_ID,
+    sysvar::{rent::Rent, rent::ID as RENT_PROGRAM_ID, Sysvar},
+};
+use spl_associated_token_account::get_associated_token_address;
+use spl_token::{
+    instruction::{initialize_mint, mint_to},
+    ID as TOKEN_PROGRAM_ID,
 };
 use std::convert::TryInto;
 
@@ -30,6 +37,7 @@ pub fn process_instruction(
             update_student_intro(program_id, accounts, name, message)
         }
         IntroInstruction::AddReply { reply } => add_reply(program_id, accounts, reply),
+        IntroInstruction::InitializeMint => initialize_token_mint(program_id, accounts),
     }
 }
 
@@ -47,21 +55,57 @@ pub fn add_student_intro(
     let initializer = next_account_info(account_info_iter)?;
     let user_account = next_account_info(account_info_iter)?;
     let reply_counter = next_account_info(account_info_iter)?;
+    let token_mint_pda = next_account_info(account_info_iter)?;
+    let mint_auth_pda = next_account_info(account_info_iter)?;
+    let user_ata = next_account_info(account_info_iter)?;
     let system_program = next_account_info(account_info_iter)?;
+    let token_program = next_account_info(account_info_iter)?;
 
-    let (pda, bump_seed) = Pubkey::find_program_address(&[initializer.key.as_ref()], program_id);
-    if pda != *user_account.key {
+    if !initializer.is_signer {
+        msg!("Missing required signature");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let (user_pda, bump_seed) =
+        Pubkey::find_program_address(&[initializer.key.as_ref()], program_id);
+    if user_pda != *user_account.key {
         msg!("Invalid seeds for PDA");
         return Err(StudentIntroError::InvalidPDA.into());
     }
+
+    msg!("Deriving mint and mint authority");
+    let (expected_mint_pda, _mint_bump) =
+        Pubkey::find_program_address(&[b"token_mint"], program_id);
+    let (expected_auth_pda, auth_bump) = Pubkey::find_program_address(&[b"token_auth"], program_id);
+
+    if *token_mint_pda.key != expected_mint_pda {
+        msg!("Incorrect token mint");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    if *mint_auth_pda.key != expected_auth_pda {
+        msg!("Incorrect token auth");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    if *user_ata.key != get_associated_token_address(initializer.key, token_mint_pda.key) {
+        msg!("Incorrect token mint");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    if *token_program.key != TOKEN_PROGRAM_ID {
+        msg!("Incorrect token program");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
     let studentinfo_discriminator = "studentinfo";
+    let account_len: usize = 1000;
     let total_len: usize =
         (4 + studentinfo_discriminator.len()) + 1 + (4 + name.len()) + (4 + message.len());
-    if total_len > 1000 {
+    if total_len > account_len {
         msg!("Data length is larger than 1000 bytes");
         return Err(StudentIntroError::InvalidDataLength.into());
     }
-    let account_len: usize = 1000;
 
     let rent = Rent::get()?;
     let rent_lamports = rent.minimum_balance(account_len);
@@ -82,7 +126,7 @@ pub fn add_student_intro(
         &[&[initializer.key.as_ref(), &[bump_seed]]],
     )?;
 
-    msg!("PDA created: {}", pda);
+    msg!("PDA created: {}", user_pda);
 
     msg!("unpacking state account");
     let mut account_data =
@@ -95,6 +139,7 @@ pub fn add_student_intro(
         return Err(ProgramError::AccountAlreadyInitialized);
     }
 
+    account_data.discriminator = studentinfo_discriminator.to_string();
     account_data.name = name;
     account_data.msg = message;
     account_data.is_initialized = true;
@@ -110,7 +155,7 @@ pub fn add_student_intro(
     let counter_rent_lamports = rent.minimum_balance(counter_len);
 
     let (counter, counter_bump) =
-        Pubkey::find_program_address(&[pda.as_ref(), "reply".as_ref()], program_id);
+        Pubkey::find_program_address(&[user_pda.as_ref(), "reply".as_ref()], program_id);
     if counter != *reply_counter.key {
         msg!("Invalid seeds for PDA");
         return Err(ProgramError::InvalidArgument);
@@ -129,7 +174,7 @@ pub fn add_student_intro(
             reply_counter.clone(),
             system_program.clone(),
         ],
-        &[&[pda.as_ref(), "reply".as_ref(), &[counter_bump]]],
+        &[&[user_pda.as_ref(), "reply".as_ref(), &[counter_bump]]],
     )?;
     msg!("reply counter created");
 
@@ -146,6 +191,27 @@ pub fn add_student_intro(
     counter_data.is_initialized = true;
     msg!("reply count: {}", counter_data.counter);
     counter_data.serialize(&mut &mut reply_counter.data.borrow_mut()[..])?;
+
+    msg!("Minting 10 tokens to User associated token account");
+    invoke_signed(
+        // Instruction
+        &mint_to(
+            token_program.key,
+            token_mint_pda.key,
+            user_ata.key,
+            mint_auth_pda.key,
+            &[],
+            10 * LAMPORTS_PER_SOL,
+        )?, // ? unwraps and returns the error if there is one
+        // Account_infos
+        &[
+            token_mint_pda.clone(),
+            user_ata.clone(),
+            mint_auth_pda.clone(),
+        ],
+        // Seeds
+        &[&[b"token_auth", &[auth_bump]]],
+    )?;
 
     Ok(())
 }
@@ -268,5 +334,106 @@ pub fn add_reply(program_id: &Pubkey, accounts: &[AccountInfo], reply: String) -
     msg!("Reply Count: {}", counter_data.counter);
     counter_data.counter += 1;
     counter_data.serialize(&mut &mut reply_counter.data.borrow_mut()[..])?;
+    Ok(())
+}
+
+pub fn initialize_token_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    // The order of accounts is not arbitrary, the client will send them in this order
+    // Whoever sent in the transaction
+    let initializer = next_account_info(account_info_iter)?;
+    // Token mint PDA - derived on the client
+    let token_mint_pda = next_account_info(account_info_iter)?;
+    // Token mint authorirty (this should be you)
+    let mint_auth_pda = next_account_info(account_info_iter)?;
+    // System program to create a new account
+    let system_program = next_account_info(account_info_iter)?;
+    // Solana Token program address
+    let token_program = next_account_info(account_info_iter)?;
+    // System account to calcuate the rent
+    let sysvar_rent = next_account_info(account_info_iter)?;
+
+    let (expected_token_mint_pda, mint_bump) =
+        Pubkey::find_program_address(&[b"token_mint"], program_id);
+
+    let (expected_mint_auth_pda, _auth_bump) =
+        Pubkey::find_program_address(&[b"token_auth"], program_id);
+
+    msg!("Token mint: {:?}", expected_token_mint_pda);
+    msg!("Mint authority: {:?}", expected_mint_auth_pda);
+
+    if expected_token_mint_pda != *token_mint_pda.key {
+        msg!("Incorrect token mint account");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    if *token_program.key != TOKEN_PROGRAM_ID {
+        msg!("Incorrect token program");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    if *mint_auth_pda.key != expected_mint_auth_pda {
+        msg!("Incorrect mint auth account");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    if *system_program.key != SYSTEM_PROGRAM_ID {
+        msg!("Incorrect system program");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    if *sysvar_rent.key != RENT_PROGRAM_ID {
+        msg!("Incorrect rent program");
+        return Err(StudentIntroError::IncorrectAccountError.into());
+    }
+
+    // Calculate the rent
+    let rent = Rent::get()?;
+    // We know the size of a mint account is 82 (remember it lol)
+    let rent_lamports = rent.minimum_balance(82);
+
+    // Create the token mint PDA
+    invoke_signed(
+        &system_instruction::create_account(
+            initializer.key,
+            token_mint_pda.key,
+            rent_lamports,
+            82, // Size of the token mint account
+            token_program.key,
+        ),
+        // Accounts we're reading from or writing to
+        &[
+            initializer.clone(),
+            token_mint_pda.clone(),
+            system_program.clone(),
+        ],
+        // Seeds for our token mint account
+        &[&[b"token_mint", &[mint_bump]]],
+    )?;
+
+    msg!("Created token mint account");
+
+    // Initialize the mint account
+    invoke_signed(
+        &initialize_mint(
+            token_program.key,
+            token_mint_pda.key,
+            mint_auth_pda.key,
+            Option::None, // Freeze authority - we don't want anyone to be able to freeze!
+            9,            // Number of decimals
+        )?,
+        // Which accounts we're reading from or writing to
+        &[
+            token_mint_pda.clone(),
+            sysvar_rent.clone(),
+            mint_auth_pda.clone(),
+        ],
+        // The seeds for our token mint PDA
+        &[&[b"token_mint", &[mint_bump]]],
+    )?;
+
+    msg!("Initialized token mint");
+
     Ok(())
 }
